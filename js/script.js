ff93e37db9b75b6d4f329178278efe5e7b807b21
Geert Uyttendaele <geertuyttendaele82@hotmail.com>
2019-11-18 13:25:32 +0100

    changed units to metrics and used forecast

--- js/script.js
+++ js/script.js
@@ -7,10 +7,11 @@
 
       const ow_user = "geertuyttendaele";
       const ow_secret_default = "aa5dac93d23ae8d8915c8d977c0631f6";
-      const api_path = "http://api.openweathermap.org/data/2.5/weather?";
+      const api_path = "http://api.openweathermap.org/data/2.5/forecast?";
       const requestedCity = `q=${cityName.value}`;
+      const units = `&units=metric`;
 
-      const url = `${api_path}${requestedCity}&appid=${ow_secret_default}`;
+      const url = `${api_path}${requestedCity}${units}&appid=${ow_secret_default}`;
       const getWeather =  async() => {
         const response = await fetch(url);
         if (response.ok) {
