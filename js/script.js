4c9439c36f6146e28b5792fc154a188f2c498b9f
Geert Uyttendaele <geertuyttendaele82@hotmail.com>
2019-11-18 12:58:00 +0100

    implemented fetch from api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 (() => {
 
-  document.getElementById("run").addEventListener("click", function(){
-      const input = document.getElementById("input");
-      console.log(input.value);
+  document.getElementById("run").addEventListener("click", function(event){
+      event.preventDefault();
+      const cityName = document.getElementById("input");
+      console.log(cityName.value);
+
+      const ow_user = "geertuyttendaele";
+      const ow_secret_default = "aa5dac93d23ae8d8915c8d977c0631f6";
+      const api_path = "http://api.openweathermap.org/data/2.5/weather?";
+      const requestedCity = `q=${cityName.value}`;
+
+      const url = `${api_path}${requestedCity}&appid=${ow_secret_default}`;
+      const getWeather =  async() => {
+        const response = await fetch(url);
+        if (response.ok) {
+          const jsonResponse =  await response.json();
+          console.log(jsonResponse);
+        }
+      }
+      getWeather();
+
+
   })
 
 
